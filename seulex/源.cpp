8af26616ec02464369c6078d0370b8b3041744c0
Fanny Chung <zhongfang1993@163.com>
2014-05-27 19:39:08 +0800

    最小化DFA测试成功

--- seulex/源.cpp
+++ seulex/源.cpp
@@ -9,12 +9,14 @@
 #include <vector>
 #include <set>
 #include <tuple>
+#include <iterator>
 #include "Node.h"
-using namespace std;
 #define EPSL 84775
+using namespace std;
 
 ifstream infile;
 ofstream outfile;
+Node* startOfDFA;
 vector<string> valNameTb;//.l自定义的变量
 //int(*trans)[256] = new int[][256];
 vector<char> alphaTB;//字符表
@@ -22,6 +24,7 @@ typedef pair<char, Node*> edge;
 typedef map<char, set<Node*>> edgesByChar;
 edgesByChar inedge;//入边表（反向表）
 edgesByChar outedge;//出边表
+vector<Node*> minDFA;//DFA'的节点集合
 
 
 typedef set<Node*> group;
@@ -69,7 +72,7 @@ void displayNodeVector(vector<Node*> d){
 	for each (Node* var in d)
 	{
 		int num = var->getNum();
-		cout << num <<(var->isTerminal())?"is Terminal":"";
+		cout << num << (var->isTerminal()) ? "is Terminal" : "";
 		cout << endl;
 		multimap<char, Node*>::const_iterator itr;
 		multimap<char, Node*> out = var->getOutAll();
@@ -95,6 +98,176 @@ bool comp(T v1, T v2)
 	else
 		return false;
 }
+
+/*
+//计算两个set的交集
+template <class T>
+set<T> Jiao(set<T> s1, set<T> s2){
+set<T> result;
+for each (T var in s1)
+{
+if (s2.find(var) != s2.end())
+result.insert(var);
+}
+return result;
+}
+//计算两个set的差集
+template <class T>
+set<T> sub(set<T> s1, set<T> s2){
+set<T> result;
+for each (T var in s1)
+{
+if (s2.find(var) == s2.end())
+result.insert(var);
+}
+return result;
+}*/
+//计算两个set的交集和差集
+template <class T>
+pair<set<T>, set<T>> jiaocha(set<T> s1, set<T> s2){
+	set<T> r1, r2;
+	for each (T var in s1)
+	{
+		if (s2.find(var) == s2.end()){
+			r1.insert(var);
+		}
+		else
+		{
+			r2.insert(var);
+		}
+	}
+	return make_pair(r1, r2);
+}
+//Hopcroft's algorithm最小化DFA,参考wikipedia，自动机理论、语言和计算导论 worst case:O(ns log n), where n is the number of states and s is the size of the alphabet.
+void miniDFA(Node* start){
+	layer P;
+	P.push_back(endStates);
+	P.push_back(nendStates);//P := {F, Q \ F};
+	layer w;
+	w.push_back(endStates);//W := {F};
+	//stack<set<Node*>> pstack;
+	//pstack.push(endStates);
+	//pstack.push(nendStates);//P := {F, Q \ F};
+
+	vector<int> deleteInvec;//记录P中要删除的元素的下标
+	while (!w.empty())//while (W is not empty) do
+	{
+		group A = w.back();//  choose and remove a set A from W
+		w.pop_back();
+		for each (char ch in alphaTB)// for each c in ∑ do
+		{
+			set<Node*> X;//let X be the set of states for which a transition on c leads to a state in A  X--ch--->A
+			set<Node*> tmp = outedge.find(ch)->second;//状态nd在ch字符对应的出边表里
+			for each (Node* nd in tmp)
+			{
+				vector<Node*>::iterator iter;//且状态nd的后继节点在A中，则把状态nd加入X
+				vector<Node*> tmp1 = nd->findNext(ch);
+				for (iter = tmp1.begin(); iter != tmp1.end(); ++iter)
+				{
+					//cout << (*iter).first << "  " << (*iter).second->getNum() << endl;
+					if (A.find(*iter) != A.end()){
+						X.insert(nd);
+					}
+				}
+			}
+			if (X.empty())
+				continue;
+			int i = 0;
+			while (i < P.size()) //for each set Y in P for which X ∩ Y is nonempty and Y \ X is nonempty do
+			{
+				if (find(deleteInvec.begin(), deleteInvec.end(), i) != deleteInvec.end()){
+					i++;
+					continue;
+				}
+				set<Node*> Y = P[i];
+				//set<Node*> jiaoset = Jiao(X, Y);
+				//set<Node*> subset = sub(Y, X);
+				pair<set<Node*>, set<Node*>> res = jiaocha(Y, X);
+				set<Node*> jiaoset = res.first;
+				set<Node*> subset = res.second;
+
+				if (jiaoset.empty() || subset.empty()){
+					i++;
+					continue;
+				}
+
+				//  replace Y in P by the two sets X ∩ Y and Y \ X
+				deleteInvec.push_back(i);
+				P.push_back(jiaoset);
+				P.push_back(subset);
+				layer::iterator itrL = find(w.begin(), w.end(), Y);
+				if (itrL != w.end()){                                  // if Y is in W
+					layer::iterator rmvw = remove(w.begin(), w.end(), Y);//      replace Y in W by the same two sets
+					w.erase(rmvw, w.end());
+					w.push_back(jiaoset);
+					w.push_back(subset);
+				}
+				else                                    //else add  the small one of X ∩ Y and Y \ X
+				{
+					if (jiaoset.size() <= subset.size())
+						w.push_back(jiaoset);
+					else
+						w.push_back(subset);
+				}
+				i++;
+			}
+		}
+	}
+	//考虑到erase效率低，新建一个P2存放P中不应该被删除的元素
+	layer P2;
+	for (int i = 0; i < P.size(); i++)
+	{
+		vector<int>::iterator itr = find(deleteInvec.begin(), deleteInvec.end(), i);
+		if (itr == deleteInvec.end())
+			P2.push_back(P[i]);
+	}
+	//找到等价类中存在起始点的集合
+	/*for each (set<Node*> var in P2)
+	{
+	for each(Node* _node in var){
+	if ()
+	}
+	}*/
+	cout << "division done!!!!!!!!\n size =" << P2.size() << "\nbegin new DFA-----------------" << endl;
+	//划分子集完成，建立新的DFA节点和连接关系
+	int i = 0;
+	typedef tuple<int, char, int> move;
+	set<move> movevec;//点-边-点的三元组的集合
+	for each (set<Node*> setOfNodes in P2)//重新建立minDFA的节点,P2中每个集合对应一个节点
+	{
+		Node* minNode = new Node(i);
+		for each (Node* nodeRpr in setOfNodes)
+		{
+			if ((!minNode->isTerminal()) && nodeRpr->isTerminal()){//如果节点集合里有一个节点是终态，则这个新的minDFA的节点是终态
+				minNode->setTerminal(true);
+			}
+			for each (char c in alphaTB)
+			{
+				Node* nextNd = nodeRpr->findNext(c)[0];//找出节点对应字符的后继，dfa节点一个字符只对应一个后继
+				//遍历P里的节点状态，找到出边的节点所在的集合的编号
+				for (int j = 0; j < P2.size(); j++)
+				{
+					if (P2[j].find(nextNd) != P2[j].end()){
+						movevec.insert(make_tuple(i, c, j));//增加映射
+						break;
+					}
+				}
+			}
+		}
+		minDFA.push_back(minNode);
+		i++;
+	}
+
+	i = 0;
+	for each (move _move in movevec)
+	{
+		minDFA[get<0>(_move)]->addOut(get<1>(_move), minDFA[get<2>(_move)]);
+		cout << "add edge " << get<0>(_move) << '\t' << get<1>(_move) << '\t' << get<2>(_move) << endl;
+	}
+	displayNodeVector(minDFA);
+	//删除dead state 和not reachable state
+}
+
 
 //接受起点，构造DFA的节点集合和连接表
 void generateDFA(Node* start){
@@ -162,7 +335,7 @@ void generateDFA(Node* start){
 				for each (Node* nd in e)   //根据该状态集合里是否有终态确定新状态是否是终态
 				{
 					bool ndisT = nd->isTerminal();
-					if(ndisT){
+					if (ndisT){
 						isTermin = true;
 						break;
 					}
@@ -191,180 +364,66 @@ void generateDFA(Node* start){
 	//删除原来的NFA所有节点
 
 	displayNodeVector(dfaNodes);
+	miniDFA(node1);
 	/*
 	for each (Node* dfaNd in dfaNodes)
 	{
 	delete dfaNd;
 	}*/
 }
-
-/*
-void generateLayer(layer ly){
-for each (group g in ly)
-{
-for each (char ch in alphaTB)
-{
-
-}
-}
-}*/
-//计算两个set的交集
-template <class T>
-set<T> Jiao(set<T> s1, set<T> s2){
-	set<T> result;
-	for each (T var in s1)
+//向输出文件写入内容
+void writeToc(){
+	outfile << "#include<iostream>\nusing namespace std;\n"
+		"const int MYERROR=1000000;"
+		"string analysis(string yytext){\n"
+		"\tint state=0;\n"
+		"\tint i=0;\n"
+		"\tchar ch=yytext[i];\n"
+		"\twhile(i<=yytext.length()){\n"
+		"\t\tswitch(state){\n";
+	for each (Node* nd in minDFA)
 	{
-		if (s2.find(var) != s2.end())
-			result.insert(var);
-	}
-	return result;
-}
-//计算两个set的差集
-template <class T>
-set<T> sub(set<T> s1, set<T> s2){
-	set<T> result;
-	for each (T var in s1)
-	{
-		if (s2.find(var) == s2.end())
-			result.insert(var);
-	}
-	return result;
-}
-//计算两个set的交集和差集
-/*
-template <class T>
-pair<set<T>, set<T>> jiaocha(set<T> s1, set<T> s2){
-pair<set<T>, set<T>> presult;
-for each ( T in s1)
-{
-if (s2.find(var) == s2.end()){
-presult->second.push_back(var);
-}
-else
-{
-presult->first.push_back(var);
-}
-}
-return presult;
-}*/
-//Hopcroft's algorithm最小化DFA,参考wikipedia，自动机理论、语言和计算导论 worst case:O(ns log n), where n is the number of states and s is the size of the alphabet.
-void miniDFA(){
-	layer P;
-	P.push_back(endStates);
-	P.push_back(nendStates);//P := {F, Q \ F};
-	layer w;
-	w.push_back(endStates);//W := {F};
-	//stack<set<Node*>> pstack;
-	//pstack.push(endStates);
-	//pstack.push(nendStates);//P := {F, Q \ F};
+		outfile << "\t\tcase " << nd->getNum() << ":\n";
+		if (nd->isTerminal()){//如果是终态,执行action
+			//string action = ;//根据节点编号找到对应的string
+			outfile << "\t\t\t\tif(i==yytext.length()){\n"
+				"\t\t\t\tbreak;\n"
+				"\t\t\t}";
+		}
 
-
-	while (!w.empty())//while (W is not empty) do
-	{
-		group A = w.back();//  choose and remove a set A from W
-		w.pop_back();
-		for each (char ch in alphaTB)// for each c in ∑ do
+		typedef multimap<char, Node*> chedge;
+		chedge outs = nd->getOutAll();
+		chedge::iterator itouts = outs.begin();
+		bool ifelse = true;
+		while (itouts != outs.end())//对于该节点的每个出边，都有一个if else
 		{
-			set<Node*> X;//let X be the set of states for which a transition on c leads to a state in A  X--ch--->A
-			set<Node*> tmp = outedge.find(ch)->second;//状态nd在ch字符对应的出边表里
-			for each (Node* nd in tmp)
-			{
-				vector<Node*>::iterator iter;//且状态nd的后继节点在A中，则把状态nd加入X
-				vector<Node*> tmp1 = nd->findNext(ch);
-				for (iter = tmp1.begin(); iter != tmp1.end(); ++iter)
-				{
-					//cout << (*iter).first << "  " << (*iter).second->getNum() << endl;
-					if (A.find(*iter) != A.end()){
-						X.insert(nd);
-					}
-				}
+			outfile << "\t\t\t";
+			if (ifelse){
+				outfile << "if";
+				ifelse = false;
 			}
-			layer::iterator pitr = P.begin();
-
-			while(pitr!=P.end()) //for each set Y in P for which X ∩ Y is nonempty and Y \ X is nonempty do
-			{
-				set<Node*> Y = *pitr;
-				set<Node*> jiaoset = Jiao(X, Y);
-				set<Node*> subset = sub(Y, X);
-
-				if (jiaoset.empty() || subset.empty()){
-					pitr++;
-					continue;
-				}
-
-				//layer::iterator rmv=remove(P.begin(), P.end(), Y);//  replace Y in P by the two sets X ∩ Y and Y \ X
-				//P.erase(rmv, P.end());
-				pitr = P.erase(pitr);
-				P.push_back(jiaoset);
-				P.push_back(subset);
-				layer::iterator itrL = find(w.begin(), w.end(), Y);
-				if (itrL != w.end()){          // if Y is in W
-					layer::iterator rmvw=remove(w.begin(), w.end(), Y);//      replace Y in W by the same two sets
-					w.erase(rmvw, w.end());
-					w.push_back(jiaoset);
-					w.push_back(subset);
-				}
-				else                                    //else
-				{
-					if (jiaoset.size() <= subset.size())
-						w.push_back(jiaoset);
-					else
-						w.push_back(subset);
-				}
+			else{
+				outfile << "else if";
 			}
+			outfile << "(ch=='" << itouts->first << "'){\n"
+				"\t\t\t\tstate=" << itouts->second->getNum() << ";"
+				"\t\t\t\tbreak;\n";
+			itouts++;
+		}
+		if (outs.size() != 0){//最后的情况------------------------------------------------
+			outfile << "\t\t\telse{\n"
+				"\t\t\t\treturn \"MYERROR\";\n"
+				"\t\t\t\tbreak;\n"
+				"\t\t\t}";
 		}
 	}
-	cout << "division done!!!!!!!!\n size ="<<P.size()<<"\nbegin new DFA-----------------" << endl;
-	//划分子集完成，建立新的DFA节点和连接关系
-	int i = 0;
-	typedef tuple<int,char,int> move;
-	vector<tuple<int, char, int>> movevec;//点-边-点的三元组的集合
-	vector<Node*> minDFA;//DFA'的节点集合
-	for each (set<Node*> setOfNodes in P)//重新建立minDFA的节点
-	{
-		Node* minNode=new Node(i);
-		for each (Node* nodeRpr in setOfNodes)
-		{
-			if (nodeRpr->isTerminal()){
-				minNode->setTerminal(true);
-				continue;
-			}
-			for each (char c in alphaTB)
-			{
-				Node* nextNd = nodeRpr->findNext(c)[0];//找出节点对应字符的后继，dfa节点一个字符只对应一个后继
-				//遍历P里的节点状态，找到出边的节点所在的集合的编号
-				for (int j = 0; j < P.size(); j++)
-				{
-					if (P[j].find(nextNd) == P[j].end()){
-						movevec.push_back(make_tuple(i, c, j));//增加映射
-						cout << "add map " << i << '\t' << c << '\t' << j << endl;
-					}
-				}
-			}
-		}
-		minDFA.push_back(minNode);
-		i++;
-	}
-
-	i = 0;
-	int k = 0;
-	for each (set<Node*> setOfNodes in P)  //重新建立连接关系
-	{
-		Node* minNode = minDFA[i];
-		move tempp = movevec[k];
-		while (i == get<0>(tempp))
-		{
-			minNode->addOut(get<1>(tempp), minDFA[get<2>(tempp)]);
-			cout << "add edge " << i << '\t' << get<1>(tempp) << '\t' << get<2>(tempp) << endl;
-			k++;
-			if (k >= movevec.size())
-				break;
-			tempp = movevec[k];
-		}
-		i++;
-	}
-	displayNodeVector(minDFA);
-	//删除dead state 和not reachable state
+	outfile << "\t\t\tdefault:\n"  //default case
+		"return \"MYERROR\";\n"
+		"\t\t}"                 //end switch
+		"\t\ti++;\n"			//next char
+		"\t\tch=yytext[i];\n"
+		"\t}"                    //end while
+		"}";                 //end function
 }
 int main(){
 	Node* n1 = new Node(1);
@@ -401,10 +460,8 @@ int main(){
 
 	vector<Node*> I;
 	I.push_back(n1);
-
+	cout << "\nbegin generate DFA--------------------" << endl;
 	generateDFA(n1);
-	miniDFA();
-	
 
 	delete n1; delete n2; delete n3; delete n4; delete n5; delete n6; delete n7; delete n8;
 	//delete[]trans;
